f878449072342d49f1a41ebfe13a45b790c603e7
unknown <pedro.meireles.rocha@gmail.com>
2024-12-25 10:54:12 -0300

    adding credentials

---
+++ cypress/e2e/credentials.js
@@ -0,0 +1,6 @@
+const credentials = {
+    username: "teste2021@teste.com.br",
+    password: "teste123"
+};
+
+export default credentials;
--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,24 +1,39 @@
+import credentials from './credentials';
+
 describe('Automation Exercise', () => {
-  it('login ae', () => {
 
-    //login realizdo com usuario disponibilizado para o exercicio//
-    cy.visit('https://www.automationexercise.com/login')
-    cy.get('[data-qa="login-email"]') .type('teste2021@teste.com.br')
-    cy.get('[data-qa="login-password"]') .type('teste')
-    cy.get('[data-qa="login-button"]') .click()
+  it("invalido", () => {
+    cy.visit('https://www.automationexercise.com/login');
+    cy.get('[data-qa="login-email"]').type('teste2021@teste.com.br');
+    cy.get('[data-qa="login-password"]').type('teste');
+    cy.get('[data-qa="login-button"]').click();
+  
+  })
+  
+  it('login > search > validation', () => {
 
-    //realizando busca e incluindo produto no carrinhho
-    cy.get('.shop-menu > .nav > :nth-child(2) > a').click()
-    cy.location ('pathname') .should('equal',"/products")
-    cy.get('#search_product') .type('Premium Polo T-Shirts')
-    cy.get('#submit_search') .click()
-    cy.get('.productinfo > .btn') .click ()
-    cy.get('body') .should('contain',"Your product has been added to cart.")
-    cy.get('u') .click()
+    // Login realizado com usuário disponibilizado para o exercício //
+    cy.visit('https://www.automationexercise.com/login');
+    cy.get('[data-qa="login-email"]').type(credentials.username);
+    cy.get('[data-qa="login-password"]').type(credentials.password);
+    cy.get('[data-qa="login-button"]').click();
+
+    // Realizando busca e incluindo produto no carrinho //
+    cy.get("[href='/products']").click();
+    cy.location('pathname').should('equal', "/products");
+    cy.get('#search_product').type('Premium Polo T-Shirts');
+    cy.get('#submit_search').click();
+    cy.get('a.add-to-cart').contains('Add to cart').click();
+    cy.get('body').contains("Your product has been added to cart.");
+    cy.get('u').contains("View Cart").click();
     
-    //validando produto no carrinho
-    cy.location ('pathname') .should('equal',"/view_cart")
-    cy.get('body') .should('contain','Premium Polo T-Shirts')
+    // Validando produto no carrinho //
+    cy.location('pathname').should('equal', "/view_cart");
+    cy.get('.disabled').should('contain', '1');
+    cy.get('body').should('contain', 'Premium Polo T-Shirts');
+    cy.contains('Premium Polo T-Shirts').should('be.visible');
+    cy.contains('Rs. 1500').should('be.visible');
+    cy.contains('Proceed To Checkout').click();
 
   })
 })
